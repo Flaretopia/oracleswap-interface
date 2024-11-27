2e1daf8a70f919d16712f35b0770dfdadff3d692
rare <rarenftclub@gmail.com>
2024-11-27 15:32:44 -0800

    Revert "fix build errors"
    This reverts commit af864ba7a941719fcbbb50b97159a35f264ea568.

--- next.config.js
+++ next.config.js
@@ -167,13 +167,6 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 }
 
 
